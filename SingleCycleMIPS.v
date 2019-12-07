bf7bd6e0ffb8c7047170cc5fdb1ecda08ab8d097
zwei22 <wei851010@gmail.com>
2019-12-07 18:57:40 +0800

    Initial Commit

---
+++ SingleCycleMIPS.v
@@ -0,0 +1,71 @@
+module SingleCycleMIPS( 
+    clk,
+    rst_n,
+    IR_addr,
+    IR,
+    ReadDataMem,
+    CEN,
+    WEN,
+    A,
+    Data2Mem,
+    OEN
+);
+
+//==== in/out declaration =================================
+    //-------- processor ----------------------------------
+    input         clk, rst_n;
+    input  [31:0] IR;
+    output [31:0] IR_addr;
+    //-------- data memory --------------------------------
+    input  [31:0] ReadDataMem;  
+    output        CEN;  
+    output        WEN;  
+    output  [6:0] A;  
+    output [31:0] Data2Mem;  
+    output        OEN;  
+
+//==== reg/wire declaration ===============================
+
+
+//==== wire connection to submodule ======================
+//Example:
+//	ctrl control(
+//	.clk(clk),
+//	.rst_n(rst_n), ......
+
+//	);
+
+//==== combinational part =================================
+
+always@(*)begin
+
+
+
+
+
+end
+
+//==== sequential part ====================================
+always@(posedge clk)begin
+	
+
+end
+
+endmodule
+
+// recommend you to use submodule for easier debugging 
+//=========================================================
+//Example:
+//	module ctrl(
+//		clk,
+//		rst_n, ....
+
+
+//	);
+
+
+
+
+
+//	endmodule
+		
